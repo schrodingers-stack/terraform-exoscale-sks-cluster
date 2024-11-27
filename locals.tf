29a168be6f10464790e23e5a1239686272a9d5de
Gonçalo Heleno <goncalocheleno@atomp.eu>
2024-11-27 13:23:00 +0100

    docs: add small note on comment in locals.tf

--- locals.tf
+++ locals.tf
@@ -3,7 +3,9 @@ locals {
   # its fields.
   kubeconfig = yamldecode(resource.exoscale_sks_kubeconfig.this.kubeconfig)
 
-  # Select the default node pool if the public or private node pool is not defined.
+  # Select the default node pool to associate with the LB if it is not is not defined.
+  # NOTE: technically, the `private_lb_instance_pool_name` will always be the same as `var.private_lb_nodepool` 
+  # because it is a required variable.
   public_lb_instance_pool_name  = var.public_lb_nodepool != null ? var.public_lb_nodepool : keys(var.nodepools)[0]
   private_lb_instance_pool_name = var.private_lb_nodepool != null ? var.private_lb_nodepool : keys(var.nodepools)[0]
 }
